97e00589f6de3ed3768f297dad8605681daaf94e
Prabhuling Biradar <prabhuling66@gmail.com>
2022-03-20 10:58:49 +0530

    Added model with datastore example

---
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.BookStore.Models;
+using Webgentle.BookStore.Repository;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> Searchbooks(string bookName, string author)
+        {
+            return _bookRepository.SearchBook(bookName,author);
+        }
+    }
+}
---
+++ Webgentle.BookStore/Webgentle.BookStore/Repository/BookRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.BookStore.Models;
+
+namespace Webgentle.BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataStore();
+        }
+
+        public BookModel GetBookById(int id)
+        {
+            return DataStore().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<BookModel> SearchBook(string title, string author)
+        {
+            return DataStore().Where(x => x.Title.Contains(title) || x.Author.Contains(author)).ToList();
+        }
+
+        private List<BookModel> DataStore()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){ Id=1, Title="MVC", Author="Nitish"},
+                new BookModel(){ Id=2, Title="ASP.NET", Author="Nitish"},
+                new BookModel(){ Id=3, Title="WEB API", Author="Monika"},
+                new BookModel(){ Id=4, Title="Angular", Author="Prabhu"},
+                new BookModel(){ Id=5, Title="Java", Author="Prabhu"}
+            };
+        }
+    }
+}
